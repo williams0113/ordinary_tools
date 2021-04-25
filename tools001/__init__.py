93355bdbeec0d9497db45f10b8e5a18ad406da5b
weilinhua <weilinhua@bluemoon.com.cn>
2021-04-25 16:39:43 +0800

    第一次提交

---
+++ tools001/__init__.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# author：williams time:2021/4/25
---
+++ tools001/deal_with_datas.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# author：williams time:2021/4/25
+
+
+
+import os
+
+with open(r"a.txt","a") as f:
+    f.write("hello\ni\nlove\nu\n")
